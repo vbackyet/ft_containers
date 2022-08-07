78b319c992c3c6849da7127601744612148bcfdb
Vaemond Backyet <vbackyet@ev-e5.kzn.21-school.ru>
2022-08-07 19:11:53 +0300

    iterators begin

--- iterator.hpp
+++ iterator.hpp
@@ -1,11 +1,18 @@
 #pragma once
-
+#include <iostream>
+#include <iterator>
 
 
 namespace ft
 {
-    class Iterator
+    template <class It>
+    class LegacyRandomAccessIterator
     {
+        private:
+        public:
+         typedef typename std::iterator_traits<It>::value_type value_type;
+         typedef typename std::iterator_traits<It>::difference_type difference_type;
+         typedef typename std::iterator_traits<It>::reference reference;
 
     };
 }
