5a371edf413c903fb1ee98ff58f6591d6022eb36
Railia <watrailya2019@gmail.com>
2022-08-27 22:15:21 +0300

    msp

--- map.hpp
+++ map.hpp
@@ -1,7 +1,13 @@
 #pragma once
-
+#include <functional>
 namespace ft
 {
+    template<
+
+    class Key,
+    class T,
+    class Compare = std::less<Key>,
+    class Allocator = std::allocator<std::pair<const Key, T> >>
     class Map
     {
     private:
